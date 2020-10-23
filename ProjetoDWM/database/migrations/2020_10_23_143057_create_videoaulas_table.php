737362881094ad1e6ac65ec0712522a169cfaa61
Diogo Oliveira <diogoafoliveira@ua.pt>
2020-10-23 15:51:04 +0100

    BD

---
+++ ProjetoDWM/database/migrations/2020_10_23_143057_create_videoaulas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVideoaulasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('videoaulas', function (Blueprint $table) {
+            $table->bigInteger('id');
+            //$table->bigInteger('id_disciplina');
+            $table->String('nome');
+            $table->String('descricao');
+            $table->String('url');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('videoaulas');
+    }
+}
