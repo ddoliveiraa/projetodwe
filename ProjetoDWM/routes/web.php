bda612fdbb89d3d8a789680e1f9c5076ff2bf79d
Diogo Oliveira <diogoafoliveira@ua.pt>
2020-10-27 17:02:18 +0000

    Autenticação

--- ProjetoDWM/routes/web.php
+++ ProjetoDWM/routes/web.php
@@ -32,3 +32,7 @@ Route::get('/registo', function () {
 Route::get('/login', function () {
     return view('login');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
