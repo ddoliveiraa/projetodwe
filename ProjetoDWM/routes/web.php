d45c6c8d8d19d45014744be00293658c010b29e5
Diogo Oliveira <diogoafoliveira@ua.pt>
2020-11-02 18:01:22 +0000

    ajuste de rederecionamento

--- ProjetoDWM/routes/web.php
+++ ProjetoDWM/routes/web.php
@@ -14,11 +14,14 @@ use App\Http\Controllers\CadeirasController;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+
 
 Route::get('/', function () {
     return view('index');
 });
+*/
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/cadeira', function () {
     return view('cadeiraa');
